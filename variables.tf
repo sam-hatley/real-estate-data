a07e835b50d3314cb08cf1a29f5c11e534f50578
Sam Hatley <sam+github@samhatley.com>
2023-03-19 11:35:03 +0000

    Update Terraform

--- variables.tf
+++ variables.tf
@@ -4,28 +4,22 @@ locals {
 
 variable "project" {
   description = "real-estate-data"
-  default = "real-estate-data-377813"
+  default     = "real-estate-data-377813"
 }
 
 variable "region" {
   description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "europe-west2"
-  type = string
+  default     = "europe-west2"
+  type        = string
 }
 
 variable "storage_class" {
-    description = "Storage class type for bucket."
-    default = "STANDARD"  
+  description = "Storage class type for bucket."
+  default     = "STANDARD"
 }
 
 variable "RM_DATASET" {
   description = "BigQuery Dataset that raw data from GSC will be written to"
-  type = string
-  default = "rm_data"
-}
-
-variable "HMLR_DATASET" {
-  description = "BigQuery Dataset that raw data from GSC will be written to"
-  type = string
-  default = "hmlr_data"
+  type        = string
+  default     = "rm_data"
 }
